6af5990acce7aa9bd8c7d03d1347c2da1abd8813
MichaelD1B <102457@glr.nl>
2025-01-24 07:59:13 +0000

    main

---
+++ scripts/webcam.js
@@ -0,0 +1,12 @@
+
+document.getElementById('camera1').addEventListener('click', function() {
+    document.getElementById('cameraDisplay').src = 'camera1.jpg';
+});
+
+document.getElementById('camera2').addEventListener('click', function() {
+    document.getElementById('cameraDisplay').src = 'camera2.jpg';
+});
+
+document.getElementById('camera3').addEventListener('click', function() {
+    document.getElementById('cameraDisplay').src = 'camera3.jpg';
+});
